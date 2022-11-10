0fd61ae6b4f0a531015de5eba6223c2c9bbf5334
Newbiecodeweaver <carlosgilino@gmail.com>
2022-11-10 14:12:18 +0100

    about me section completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
 
 <!-- Works section -->
 
-<div id="works" class="{{'p-5'}}">
+<section id="works" class="{{'p-10'}}">
 
   <div id="block-works" class="{{'text-center : mt-5'}}">
 
@@ -46,13 +46,30 @@
             <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </a>
     </div>
-</div>
+  </div>
 
 
 
 <div class="w-full m-2 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
   <a href="#">
       <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="">
+  </a>
+  <div class="p-5">
+      <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+      </a>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Read more
+          <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      </a>
+  </div>
+</div>
+
+
+<div class="w-full m-2 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+  <a href="#">
+      <img class="rounded-t-lg" src="{{ asset('resources/images/foto-perfil.png') }}" alt="">
   </a>
   <div class="p-5">
       <a href="#">
@@ -83,37 +100,49 @@
   </div>
 </div>
 
-
-<div class="w-full m-2 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-  <a href="#">
-      <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="">
-  </a>
-  <div class="p-5">
-      <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-      </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-      <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Read more
-          <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-      </a>
-  </div>
 </div>
 
-</div>
-
-</div>
+</section>
 
 <!-- About me section -->
 
-<div id="about-me">
+<section id="about-me" class="bg-slate-100 pt-6">
 
-</div>
+  <div id="block-works" class="{{'text-center : mt-5'}}">
+
+    <h2 class="{{ 'text-3xl : font-bold' }}">Sobre mí</h2>
+    <div class="{{'mt-2'}}">Alguna información sobre mí</div>
+
+  </div>
+
+  <div id="info-card" class="{{'flex : flex-col : items-center : justify-center : w-full : pt-12 : pl-12 : pr-12 : pb-16 : md:flex-row' }}">
+    
+    
+    
+    <div class="w-96"><img src="{{url('/../resources/images/foto-perfil-web.png');}}" alt="My logo" width="250px" />
+    </div>
+    <div class="w-96">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero assumenda corrupti soluta ipsam deleniti nobis nisi, architecto, asperiores quo mollitia quas omnis excepturi necessitatibus similique ipsa nemo doloribus quos. Quisquam?</div>
+  
+  
+  </div>
+  
+
+
+  </div>
+
+</section>
 
 <!-- Contact me section -->
 
 <div id="contact-me">
+  <section id="contact">
 
+    <div id="block-contact" class="{{'text-center : mt-5'}}">
+  
+      <h2 class="{{ 'text-3xl : font-bold' }}">Contacto</h2>
+      <div class="{{'mt-2'}}">Contacta conmigo</div>
+  
+    </div>
 </div>
 
 @endsection
