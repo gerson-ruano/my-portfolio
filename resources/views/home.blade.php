73c8527f71cbaac3f0564c5612afb940ccd10b30
Newbiecodeweaver <carlosgilino@gmail.com>
2022-11-12 10:27:16 +0100

    code clean up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,7 @@
       Learn more
       <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </a>
-</div>
-  
+  </div>
 </div>
 
 <!-- Works section -->
@@ -47,7 +46,6 @@
         </a>
     </div>
   </div>
-
 
 
 <div class="w-full m-2 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -97,8 +95,8 @@
           Read more
           <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </a>
+   </div>
   </div>
-</div>
 
 </div>
 
@@ -117,17 +115,11 @@
 
   <div id="info-card" class="{{'flex : flex-col : items-center : justify-center : w-full : pt-12 : pl-12 : pr-12 : pb-16 : md:flex-row' }}">
     
-    
-    
     <div class="w-96"><img src="{{url('/../resources/images/foto-perfil-web.png');}}" alt="My logo" width="250px" />
     </div>
     <div class="w-96">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero assumenda corrupti soluta ipsam deleniti nobis nisi, architecto, asperiores quo mollitia quas omnis excepturi necessitatibus similique ipsa nemo doloribus quos. Quisquam?</div>
   
-  
   </div>
-  
-
-
   </div>
 
 </section>
