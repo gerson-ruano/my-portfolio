d01c6e68d05df175c2b16d499f22b3e6b5cb48d3
Newbiecodeweaver <carlosgilino@gmail.com>
2022-11-14 12:06:37 +0100

    categories table created

--- database/migrations/2022_11_14_110238_create_works_table.php
+++ database/migrations/2022_11_14_110238_create_works_table.php
@@ -15,25 +15,7 @@ return new class extends Migration
     {
         Schema::create('works', function (Blueprint $table) {
             $table->id();
-            $table->string('name',30);
-            $table->text('description',135);
-            $table->string('demo_link');
-            $table->string('repo_link');
-            $table->string('img_url');
             $table->timestamps();
-
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')
-            ->references('id')->on('users')
-            ->onDelete('set null');
-
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')
-            ->references('id')->on('categories')
-            ->onDelete('set null');
-
-
-
         });
     }
 
