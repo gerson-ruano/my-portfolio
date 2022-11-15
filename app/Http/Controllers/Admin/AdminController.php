36de7d137088124d4c80fa240e721d4448661f7a
Newbiecodeweaver <carlosgilino@gmail.com>
2022-11-15 10:21:01 +0100

    created AdminController with basic methods: index, create, edit, update and destroy

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Work;
+use Illuminate\Http\Request;
+
+
+class AdminController extends Controller
+{
+    public function adminIndex() {
+
+        $workModel = new Work();
+        $works = $workModel->getAllWorks();
+        return view('admin',compact('works'));
+
+    }
+
+    public function adminCreateWork() {
+
+        return "show form to add new work";
+
+    }
+
+    public function adminEditWork(work $Work) {
+
+        return "get work and edit by form";
+
+    }
+
+    public function adminUpdateWork() {
+
+        return "get new work data from form and update it";
+
+    }
+
+    public function adminDestroyWork(work $Work) {
+
+        return "get work and remove it from database";
+    }
+}
