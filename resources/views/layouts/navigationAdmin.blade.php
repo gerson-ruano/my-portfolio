9a923327267b8008390826bd7cbbf9b8e8e4ede8
Newbiecodeweaver <carlosgilino@gmail.com>
2022-11-16 11:30:36 +0100

    route added to 'new work' button

--- resources/views/layouts/navigationAdmin.blade.php
+++ resources/views/layouts/navigationAdmin.blade.php
@@ -6,9 +6,9 @@
       <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
   </a>
   <div class="flex md:order-2">
-    <button type="button" class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2 text-center inline-flex items-center mr-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
+    <a href="{{route('admin.create')}}" class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2 text-center inline-flex items-center mr-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
       <svg class="w-6 h-6 pr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
       <span> NUEVO PROYECTO</span>
-  </div>
+  </a>
   </div>
 </nav>
