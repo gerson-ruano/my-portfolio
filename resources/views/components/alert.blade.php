6114a22b0c65fecd89f2c8f9a08219d4082acc2a
Newbiecodeweaver <carlosgilino@gmail.com>
2022-11-21 13:05:47 +0100

    alert component created

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,3 @@
+<div class="p-4 text-sm text-gray-700 bg-gray-100 rounded-lg dark:bg-gray-700 dark:text-gray-300" role="alert">
+    <span class="font-medium">{{$message}}</span> {{$description}}
+  </div>
