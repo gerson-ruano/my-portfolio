05a3a6b4aa0ba4aab1e8eff75f4651db7f74d73b
Newbiecodeweaver <carlosgilino@gmail.com>
2022-11-22 13:02:34 +0100

    HomeController gets user information to show on view

--- app/Http/Controllers/HomeControler.php
+++ app/Http/Controllers/HomeControler.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Work;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeControler extends Controller
 {
     public function __invoke()
     {
+        $user = Auth::user();
         $workModel = new work();
         $works = $workModel->getAllWorks();
-        return view('home',compact('works'));
+        return view('home',compact('works', 'user'));
     }
 }
