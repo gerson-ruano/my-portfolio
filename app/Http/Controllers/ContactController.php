6fe36516f1aa75e8ad3f3fe3763fee7f06cff7e6
Newbiecodeweaver <carlosgilino@gmail.com>
2022-11-29 13:50:00 +0100

    new controller: ContactController

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function storeContact(Request $request) 
+    {
+
+        $request->validate(
+        [
+            'email' => 'required|email',
+            'subject' => 'nullable',
+            'message' =>  'required|max:1200',
+        ]);
+
+        $input = $request->all();
+    
+        Contact::create($input);
+
+        Mail::send('contact', array(
+            'email' => $input['email'],
+            'subject' => $input['subject'],
+            'message' => $input['message'],
+        ), function($message) use ($request){
+            $message->from($request->email);
+            $message->to('carlosgilino@gmail.com', 'Carlos Martinez')
+            ->subject($request->get('subject'));
+        });
+
+        return redirect()->route('admin.index')->with(
+            [
+                'type' => '¡Email enviado!',
+                'msg' =>'Tu email se ha enviado correctamente'
+            ]);
+
+
+        
+    }
+}
