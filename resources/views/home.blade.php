2be6115c878b8c3042786b888ee97be408856a49
Newbiecodeweaver <carlosgilino@gmail.com>
2022-11-30 15:43:45 +0100

    contact form view modified to show alert according with submit  result

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,6 @@
 
 @include('layouts.navigation')
 
-@if (Session::has('msg'))
-
-@include('components.alert')
-
-@endif  
-
 <!-- Header image -->
 
 <div class="{{ 'bg-gradient-to-r from-indigo-800 to-pink-500' }}" id="header-img">
@@ -181,10 +175,17 @@
     </div>
   </div>
 
-
   <div id="contact-form" class="p-24">
+
+    @if (Session::has('msg'))
+
+    @include('components.contact-alert')
+  
+    @endif  
+
     <form method="POST" action="#contact" class="space-y-8">
       @csrf
+      @honeypot
       <div>
           <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tu email</label>
           <input type="email" id="email" name="email" value="{{ old('email') }}" @error('email') is-invalid @enderror class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light">
