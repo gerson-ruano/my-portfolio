8332844b7b391e7caaba062475e7338df996b9d3
Newbiecodeweaver <carlosgilino@gmail.com>
2022-12-08 19:08:04 +0100

    demo and repo links changed to not required

--- resources/views/adminEdit.blade.php
+++ resources/views/adminEdit.blade.php
@@ -58,14 +58,14 @@
               <div class="mb-6 flex space-x-5 w-full">
                 <div class="flex flex-col w-full">
                   <label for="demo_link" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enlace demo</label>
-                  <input type="text" id="demo_link" name="demo_link" value="{{$work->demo_link}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('demo_link') is-invalid @enderror" placeholder="Enlace demo" required>
+                  <input type="text" id="demo_link" name="demo_link" value="{{$work->demo_link}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('demo_link') is-invalid @enderror" placeholder="Enlace demo">
                   @error('demo_link')
                   <div class="alert alert-danger text-red-600">{{ $message }}</div>
               @enderror
                 </div>
                 <div class="flex flex-col w-full">
                   <label for="repo_link" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enlace repositorio</label>
-                  <input type="text" id="repo_link" name="repo_link" value="{{$work->repo_link}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('repo_link') is-invalid @enderror" placeholder="Enlace repositorio" required>
+                  <input type="text" id="repo_link" name="repo_link" value="{{$work->repo_link}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('repo_link') is-invalid @enderror" placeholder="Enlace repositorio">
                   @error('repo_link')
                   <div class="alert alert-danger text-red-600">{{ $message }}</div>
               @enderror
